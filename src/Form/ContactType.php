a1643787e7f51415586c130eb5e672d718c72e18
admin <admin@ETUDIANT.local>
2020-07-21 16:47:53 +0200

    implement contact form

---
+++ src/Form/ContactType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\UI\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\AbstractType;
+
+class ContactType extends AbstractType
+{
+
+}
+
